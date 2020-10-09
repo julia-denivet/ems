3012c2d6f039e32a13a549b509f56dd9e5dda25d
julia-denivet <julia.denivet@laplateforme.io>
2020-10-09 16:26:28 +0200

    Form add-user add-ems envoi data fetch

--- ems/assets/js/app.js
+++ ems/assets/js/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Form_connexion from './component/form_connexion';
+import Form_add_ems from './component/form_add_ems';
 
 
-ReactDOM.render(<Router><Form_connexion /></Router>, document.getElementById('root'));
+
+ReactDOM.render(<Router><Form_add_ems/></Router>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
